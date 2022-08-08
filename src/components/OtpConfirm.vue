28795a04f7aa15076befe682a54258b229492efc
oleksandr.syvash <exortwot@gmail.com>
2022-08-08 03:21:50 +0200

    added otp confirm form

---
+++ src/components/OtpConfirm.vue
@@ -0,0 +1,81 @@
+<script>
+// This component must send an OTP code (type: Number) to server for verifying some operations
+
+export default {
+  name: 'OtpConfirm',
+  props: {
+    actionType: {
+      required: true,
+    },
+    moneyTransferId: {
+      required: false,
+    },
+    creditId: {
+      required: false,
+    },
+  },
+  data() {
+    return {
+      otp: '',
+    };
+  },
+  methods: {
+    async submit() {
+      if (this.actionType === 'login') {
+        await this.$httpClient.post('/api/login/approve-otp', {
+          otp: this.otp,
+        });
+      } else if (this.actionType === 'signMoneyTransfer') {
+        await this.$httpClient.post('/api/v1/documents/money-transfer/send-sms', {
+          transferId: this.moneyTransferId,
+          otp: this.otp,
+        });
+      } else if (this.actionType === 'signCredit') {
+        await this.$httpClient.post('/api/v1/documents/credit/send-sms', {
+          orderId: this.creditId,
+          otp: this.otp,
+        });
+      }
+    },
+    async resend() {
+      if (this.actionType === 'login') {
+        await this.$httpClient.post('/api/login/send-sms');
+      } else if (this.actionType === 'signMoneyTransfer') {
+        await this.$httpClient.post('/api/v1/documents/money-transfer/send-sms', {
+          transferId: this.moneyTransferId,
+        });
+      } else if (this.actionType === 'signCredit') {
+        await this.$httpClient.post('/api/v1/documents/credit/send-sms', {
+          orderId: this.creditId,
+        });
+      }
+    },
+  },
+  async beforeMount() {
+    await this.resend();
+  },
+};
+</script>
+
+<template>
+  <form @submit.prevent="submit">
+    <p v-if="actionType === 'signMoneyTransfer'">Sign money transfer operation</p>
+    <p v-else-if="actionType === 'signCredit'">Sign document to receive your credit</p>
+
+    <label for="otp">
+      Code from sms
+
+      <input
+        id="otp"
+        name="otp"
+        type="text"
+        v-model="otp"
+        placeholder="Type it here"
+      >
+    </label>
+
+    <button @click="resend">Resend sms</button>
+  </form>
+</template>
+
+<style></style>
